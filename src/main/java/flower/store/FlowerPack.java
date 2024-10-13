5295b929781831a6bf6fa9f5f1c8f36fa3fcd696
Sirska-Mariia <149333906+Sirska-Mariia@users.noreply.github.com>
2024-10-13 23:48:19 +0300

    code added

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -3,4 +3,13 @@ package flower.store;
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+    public double getPrice() {
+       return flower.getPrice() * quantity;
+    }
+    public static void main(String[] args) {
+    
+    }
+
 }
+
+
